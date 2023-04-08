cc37591e5ae0a2c0a6fc4de75ccc9b26b8091321
hwiju yeom <yeom456@gmail.com>
2023-04-08 15:05:00 +0900

    [Feat]: csv 스프레드시트에서 데이터를 가져와 사용자 정의 코드로 반환하고 최종 코드릴 데이터베이스에 저장하는 서비스 연습 초기 세팅작업
    출처 : https://spring.io/guides/gs/batch-processing/

---
+++ spring-batch-Practice/src/main/java/com/spring/batch/practice/springbatchpractice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.batch.practice.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPracticeApplication.class, args);
+    }
+
+}
---
+++ spring-batch-Practice/src/test/java/com/spring/batch/practice/springbatchpractice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.batch.practice.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
