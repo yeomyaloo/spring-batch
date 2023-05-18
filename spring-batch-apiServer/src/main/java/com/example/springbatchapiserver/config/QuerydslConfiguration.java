7b9f875607653ccf5325c9acf2bfcc40474c430c
hwiju yeom <yeom456@gmail.com>
2023-05-18 18:37:42 +0900

    Create QuerydslConfiguration.java

---
+++ spring-batch-apiServer/src/main/java/com/example/springbatchapiserver/config/QuerydslConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.springbatchapiserver.config;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.persistence.*;
+
+public class QuerydslConfiguration {
+
+
+    @Configuration
+    public class QueryDslConfig {
+
+        @PersistenceContext
+        private EntityManager entityManager;
+
+        @Bean
+        public JPAQueryFactory jpaQueryFactory(){
+            return new JPAQueryFactory(this.entityManager);
+        }
+
+    }
+}
